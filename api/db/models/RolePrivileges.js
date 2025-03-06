47b54bad6beaca4c93d718247006bc2ae2d6a554
Alp Salcıoğlu <alpsalcioglu10@gmail.com>
2025-03-06 15:14:19 +0300

    routing altyapısı kuruldu#2

---
+++ api/db/models/RolePrivileges.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    role_id: {type: mongoose.Schema.Types.ObjectId, required:true},
+    permission: {type: String, required: true},
+    created_by: {type: mongoose.Schema.Types.ObjectId, required: true}
+},{
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
+
+
+class RolePrivileges extends mongoose.Model{
+
+}
+
+
+schema.loadClass(RolePrivileges);
+module.exports = mongoose.model('RolePrivileges',schema);
